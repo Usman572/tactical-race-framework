747ff88fca9400d18de8f2cf896c1e6b0edd7cf9
usman Ali <usmansky72472@gmail.com>
2026-03-25 01:15:35 +0500

    chore: sanitized frontend repository (all backend elements removed)

--- backend-standalone/routes/missionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDailyMissions } = require('../controllers/missionController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/', protect, getDailyMissions);
-
-module.exports = router;
